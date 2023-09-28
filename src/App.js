9ec3376c402955b23c64bac75ee003d694b670ec
Egnoel <70013986+Egnoel@users.noreply.github.com>
2023-09-28 21:29:45 +0100

    mobile design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,70 @@
 import "./App.css";
+import arrow from "./assets/icon-arrow.svg";
 
 function App() {
   return (
-    <div className="App">
-      <p>Age calculator</p>
+    <div className="bg-[#F0F0F0] sm:min-h-screen h-screen flex items-center justify-center">
+      <div className="w-[90%] bg-white h-[70%] flex items-center justify-center rounded-t-[24px] rounded-bl-[24px] rounded-br-[100px]">
+        <div className="w-[90%] h-[85%] flex flex-col justify-between">
+          <div className="flex flex-row w-full gap-2">
+            <div className="flex flex-col w-1/3 gap-1">
+              <span>Day</span>
+              <input
+                type="number"
+                name="day"
+                id="day"
+                placeholder="DD"
+                className="w-[90%] h-8 rounded-md border"
+              />
+            </div>
+            <div className="flex flex-col w-1/3 gap-1">
+              <span>Month</span>
+              <input
+                type="number"
+                name="month"
+                id="month"
+                placeholder="MM"
+                className="w-[90%] h-8 rounded-md border"
+              />
+            </div>
+            <div className="flex flex-col w-1/3 gap-1">
+              <span>Year</span>
+              <input
+                type="number"
+                name="year"
+                id="year"
+                placeholder="YYYY"
+                className="w-[90%] h-8 rounded-md border"
+              />
+            </div>
+          </div>
+          <div className="flex flex-row items-center w-full">
+            <hr className="border-t border-solid h-[1px] w-[40%] border-[#DCDCDC]" />
+            <button
+              type="button"
+              className="rounded-full w-[20%] h-16 bg-[#854DFF] flex items-center justify-center hover:bg-[#151515]"
+            >
+              <img src={arrow} alt="Arrow" className="w-1/2 h-1/2" />
+            </button>
+            <hr className="border-t border-solid h-[1px] w-[40%] border-[#DCDCDC]" />
+          </div>
+          <div className="flex flex-col w-full">
+            <div className="flex flex-row gap-1">
+              <span className="text-[#854DFF]">38</span>
+              <span>years</span>
+            </div>
+            <div className="flex flex-row gap-1">
+              <span className="text-[#854DFF]">3</span>
+              <span>months</span>
+            </div>
+            <div className="flex flex-row gap-1">
+              <span className="text-[#854DFF]">26</span>
+              <span>days</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
